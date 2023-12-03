c8b04b1ed68346a860cb2d59cbe80080b31fd20c
Patrick Klitzke <philologos14@gmail.com>
2023-12-03 22:16:46 +0900

    day 2 part 2

--- day3/two.ts
+++ day3/two.ts
@@ -8,46 +8,80 @@ function main() {
 
   let sum = 0;
 
-  let names = [
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
-  ];
+  let arr: Array<Array<string>> = [];
 
   for (const line of lines) {
     // Process each line here
-    let items = [];
-    for (let i = 0; i < line.length; i++) {
-      if ("123456789".includes(line[i])) {
-        items.push(line[i]);
-      }
-
-      for (let j = 0; j < names.length; j++) {
-        let word = line.substring(i, i + names[j].length);
-        if (word === names[j]) {
-          console.log("true", word);
-          items.push(`${j + 1}`);
-        }
-      }
-    }
-
+    let items = line.split("");
     if (items.length > 0) {
-      let num = items[0] + items[items.length - 1];
-      console.log("val: ", num, items);
-      let val = parseInt(num);
-      sum += val;
+      arr.push(items);
     }
-
-    console.log("SUM: ", sum);
   }
 
   console.log("File reading finished.");
+  console.log(arr);
+  console.log("SUM: " + sum);
+
+  let yLength = arr.length;
+  let xLength = arr[0].length;
+
+  type StarValuesType = {
+    [key: number]: {
+      [key: number]: number[];
+    };
+  };
+
+  const starValues: StarValuesType = {};
+
+  function addNumber(y: number, x: number, num: number) {
+    if (x < 0 || x >= xLength) return;
+    if (y < 0 || y >= yLength) return;
+    if ("*" === arr[y][x]) {
+      starValues[y][x].push(num);
+    }
+  }
+
+  for (let y = 0; y < arr.length; y++) {
+    starValues[y] = {};
+    for (let x = 0; x < arr[y].length; x++) {
+      starValues[y][x] = [];
+    }
+  }
+
+  for (let y = 0; y < arr.length; y++) {
+    for (let x = 0; x < arr[y].length; x++) {
+      if ("0123456789".includes(arr[y][x])) {
+        let startX = x;
+        let number = arr[y][x];
+
+        while (x + 1 < arr[y].length && "0123456789".includes(arr[y][x + 1])) {
+          number += arr[y][x + 1];
+          x++;
+        }
+
+        let myNumber = parseInt(number);
+
+        addNumber(y, startX - 1, myNumber);
+        addNumber(y, x + 1, myNumber);
+
+        for (let x1 = startX - 1; x1 <= x + 1; x1++) {
+          addNumber(y - 1, x1, myNumber);
+          addNumber(y + 1, x1, myNumber);
+        }
+      }
+    }
+  }
+
+  for (let y = 0; y < arr.length; y++) {
+    for (let x = 0; x < arr[y].length; x++) {
+      if (starValues[y][x].length == 2) {
+        console.log("add", starValues[y][x][0], starValues[y][x][1]);
+        sum += starValues[y][x][0] * starValues[y][x][1];
+      }
+    }
+  }
+
+  console.log("SUM : " + sum);
 }
 
 main();
