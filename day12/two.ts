650ef3bf54e7dbe3849ecd067eda2fbb5ac1f0cf
Patrick Klitzke <philologos14@gmail.com>
2023-12-12 22:19:46 +0900

    solve part 2

--- day12/two.ts
+++ day12/two.ts
@@ -1,6 +1,82 @@
 import * as fs from "fs";
 
-let NUM = 1_000_000;
+function calcDamage(
+  leftPos: number,
+  rightPos: number,
+  str: string,
+  arr: Array<number>,
+  res: Map<string, number>,
+): number {
+  if (rightPos === arr.length) {
+    return 0;
+  }
+
+  if (leftPos + arr[rightPos] > str.length) {
+    return 0;
+  }
+
+  for (let x = 0; x < arr[rightPos]; x++) {
+    if (str[leftPos + x] === ".") {
+      return 0;
+    }
+  }
+
+  if (leftPos + arr[rightPos] == str.length) {
+    return calc(leftPos + arr[rightPos], rightPos + 1, str, arr, res);
+  }
+
+  if (
+    str[leftPos + arr[rightPos]] == "?" ||
+    str[leftPos + arr[rightPos]] == "."
+  ) {
+    return calc(leftPos + arr[rightPos] + 1, rightPos + 1, str, arr, res);
+  } else {
+    return 0;
+  }
+}
+
+function calc(
+  leftPos: number,
+  rightPos: number,
+  str: string,
+  arr: Array<number>,
+  res: Map<string, number>,
+): number {
+  let keyVal = `${leftPos},${rightPos}`;
+
+  let el = res.get(keyVal);
+
+  if (el != null) {
+    return el;
+  }
+
+  if (leftPos == str.length) {
+    if (rightPos == arr.length) {
+      // console.log("ONE");
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  if (leftPos > str.length) {
+    return 0;
+  }
+
+  let sol1 = 0;
+  let sol2 = 0;
+
+  if (str[leftPos] === "." || str[leftPos] === "?") {
+    sol1 = calc(leftPos + 1, rightPos, str, arr, res);
+  }
+
+  if (str[leftPos] === "#" || str[leftPos] === "?") {
+    sol2 = calcDamage(leftPos, rightPos, str, arr, res);
+  }
+
+  res.set(keyVal, sol1 + sol2);
+  return sol1 + sol2;
+}
 
 function main() {
   const filePath = process.argv[2]; // Replace with the path to your file
@@ -8,70 +84,30 @@ function main() {
   const fileContent = fs.readFileSync(filePath, "utf-8");
   const lines = fileContent.split("\n").filter((x) => x.trim().length > 0);
 
-  let arr: Array<Array<string>> = [];
-  for (const line of lines) {
-    arr.push(line.split(""));
-  }
-
-  // create type cordinates
-  type Cordinates = {
-    x: number;
-    y: number;
-  };
-
-  let cordinateArr: Array<Cordinates> = [];
-
-  for (let y = 0; y < arr.length; y++) {
-    for (let x = 0; x < arr[y].length; x++) {
-      if (arr[y][x] === "#") {
-        cordinateArr.push({ y, x });
-      }
-    }
-  }
-
-  cordinateArr = cordinateArr.sort((a, b) => {
-    return a.x - b.x;
-  });
-
-  console.log("coord", cordinateArr);
-
-  let plus = 0;
-  let lastX = cordinateArr[0].x;
-
-  for (let i = 1; i < cordinateArr.length; i++) {
-    if (cordinateArr[i].x > lastX) {
-      plus += (cordinateArr[i].x - lastX - 1) * (NUM - 1);
-      lastX = cordinateArr[i].x;
-    }
-    cordinateArr[i].x += plus;
-  }
-
-  cordinateArr = cordinateArr.sort((a, b) => {
-    return a.y - b.y;
-  });
-
-  plus = 0;
-  let lastY = cordinateArr[0].y;
-
-  for (let i = 1; i < cordinateArr.length; i++) {
-    if (cordinateArr[i].y > lastY) {
-      plus += (cordinateArr[i].y - lastY - 1) * (NUM - 1);
-      lastY = cordinateArr[i].y;
-    }
-    cordinateArr[i].y += plus;
-  }
-
   let sum = 0;
+  for (let line of lines) {
+    let [input, numbersStr] = line.split(" ");
+    let numbers = numbersStr.split(",").map((x) => parseInt(x));
 
-  for (let a = 0; a < cordinateArr.length; a++) {
-    for (let b = a + 1; b < cordinateArr.length; b++) {
-      sum +=
-        Math.abs(cordinateArr[a].x - cordinateArr[b].x) +
-        Math.abs(cordinateArr[a].y - cordinateArr[b].y);
+    console.log("data", input, numbers);
+    let res = new Map<string, number>();
+
+    let newStr = input;
+    for (let i = 0; i < 4; i++) {
+      newStr += "?" + input;
     }
+    let newArr = numbers;
+    for (let i = 0; i < 4; i++) {
+      newArr = [...newArr, ...numbers];
+    }
+
+    let val = calc(0, 0, newStr, newArr, res);
+    console.log("RES: ", val);
+    sum += val;
+    // console.log("RS", res);
   }
 
-  console.log("RES: ", sum);
+  console.log("SUM: ", sum);
 }
 
 main();
