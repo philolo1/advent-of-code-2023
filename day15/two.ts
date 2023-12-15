ff7da2cd04941540a4259337e688414a42796c3a
Patrick Klitzke <philologos14@gmail.com>
2023-12-15 22:10:30 +0900

    part 2

--- day15/two.ts
+++ day15/two.ts
@@ -1,176 +1,94 @@
 import * as fs from "fs";
 
-let X: number;
-let Y: number;
-let lines: string[][];
+function calc(str: string): number {
+  let val = 0;
 
-function print() {
-  // return;
-  // for (let y = 0; y < Y; y++) {
-  //   console.log(lines[y].join(""));
-  // }
+  for (let ch of str) {
+    let num = ch.charCodeAt(0);
+    val += num;
+    val *= 17;
+    val %= 256;
+  }
+
+  return val;
 }
+
+type Obj = {
+  label: string;
+  num: number;
+};
 
 function main() {
   const filePath = process.argv[2]; // Replace with the path to your file
 
   const fileContent = fs.readFileSync(filePath, "utf-8");
-  lines = fileContent
+  const lines = fileContent
     .split("\n")
-    .map((x) => x.trim().split(""))
+    .map((x) => x.trim())
     .filter((x) => x.length > 0);
 
-  Y = lines.length;
-  X = lines[0].length;
+  console.log("lines", lines);
 
-  print();
+  let items = lines[0].split(",");
 
-  let myMap: Map<number, Array<number>> = new Map();
+  let box: Map<number, Obj[]> = new Map();
 
-  let loadArr: number[] = [];
-
-  for (let circle = 0; circle <= 1000; circle++) {
-    // console.log("\n GRAPH: NORTH");
-    // north
-    for (let x = 0; x < X; x++) {
-      let y = 0;
-      while (y < Y) {
-        let pos = y;
-        let counter = 0;
-        while (y < Y && "O.".includes(lines[y][x])) {
-          if ("O" === lines[y][x]) {
-            counter++;
-          }
-          lines[y][x] = ".";
-          y++;
-        }
-        counter--;
-        while (counter >= 0) {
-          lines[pos + counter][x] = "O";
-          counter--;
-        }
-        y++;
-      }
-    }
-    print();
-
-    // console.log("GRAPH WEST");
-    for (let y = 0; y < Y; y++) {
-      let x = 0;
-      while (x < X) {
-        let pos = x;
-        let counter = 0;
-        while (x < X && "O.".includes(lines[y][x])) {
-          if ("O" === lines[y][x]) {
-            counter++;
-          }
-          lines[y][x] = ".";
-          x++;
-        }
-        counter--;
-        while (counter >= 0) {
-          lines[y][pos + counter] = "O";
-          counter--;
-        }
-        x++;
-      }
-    }
-
-    print();
-
-    // console.log("\n GRAPH: SOUTH");
-    // north
-    for (let x = 0; x < X; x++) {
-      let y = Y - 1;
-      while (y >= 0) {
-        let pos = y;
-        let counter = 0;
-        while (y >= 0 && "O.".includes(lines[y][x])) {
-          if ("O" === lines[y][x]) {
-            counter++;
-          }
-          lines[y][x] = ".";
-          y--;
-        }
-        counter--;
-        while (counter >= 0) {
-          lines[pos - counter][x] = "O";
-          counter--;
-        }
-        y--;
-      }
-    }
-    print();
-
-    // console.log("\n GRAPH EAST");
-    for (let y = 0; y < Y; y++) {
-      let x = X - 1;
-      while (x >= 0) {
-        let pos = x;
-        let counter = 0;
-        while (x >= 0 && "O.".includes(lines[y][x])) {
-          if ("O" === lines[y][x]) {
-            counter++;
-          }
-          lines[y][x] = ".";
-          x--;
-        }
-        counter--;
-        while (counter >= 0) {
-          lines[y][pos - counter] = "O";
-          counter--;
-        }
-        x--;
-      }
-    }
-
-    print();
-
-    let load = 0;
-    for (let y = 0; y < Y; y++) {
-      for (let x = 0; x < X; x++) {
-        if (lines[y][x] == "O") {
-          load += Y - y;
-        }
-      }
-    }
-    console.log(circle, load);
-
-    if (circle > 500) {
-      if (myMap.get(load) == null) {
-        myMap.set(load, [circle]);
-      } else {
-        let val = myMap.get(load) as number[];
-        val.push(circle);
-        myMap.set(load, val);
-      }
-    }
-
-    loadArr.push(load);
+  for (let i = 0; i < 256; i++) {
+    box.set(i, []);
   }
 
-  let myArr = myMap.get(loadArr[loadArr.length - 1]) as number[];
-  let circleLength = myArr[myArr.length - 1] - myArr[myArr.length - 2];
+  console.log("items", items);
 
-  console.log("circleLength: ", circleLength);
-  console.log(
-    "a: ",
-    loadArr[loadArr.length - 1],
-    loadArr[loadArr.length - 1 - circleLength],
-  );
+  for (let item of items) {
+    if (item[item.length - 1] === "-") {
+      let label = item.replace("-", "");
+      console.log("LABEL", label, label.length);
+      let num = calc(label);
+      let arr = box.get(num) as Obj[];
 
-  console.log(
-    "a: ",
-    loadArr[loadArr.length - 4],
-    loadArr[loadArr.length - 4 - circleLength],
-  );
+      arr = arr.filter((x) => x.label != label);
 
-  let result = (1_000_000_000 - loadArr.length - circleLength) % circleLength;
+      box.set(num, arr);
+    } else if (item.includes("=")) {
+      let [label, val] = item.split("=");
+      let boxNumber = calc(label);
+      let num = parseInt(val);
 
-  console.log("RES: ", loadArr[loadArr.length - 1 - circleLength + result]);
+      let arr = box.get(boxNumber) as Obj[];
 
-  // 14
-  //
+      let hasFound = false;
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i].label === label) {
+          arr[i].num = num;
+          hasFound = true;
+          break;
+        }
+      }
+
+      if (!hasFound) {
+        arr.push({ label, num });
+      }
+
+      box.set(boxNumber, arr);
+    } else {
+      throw Error("Cannot parse :" + item);
+    }
+  }
+
+  let sum = 0;
+  for (let i = 0; i < 256; i++) {
+    let arr = box.get(i) as Obj[];
+
+    for (let j = 0; j < arr.length; j++) {
+      sum += (i + 1) * arr[j].num * (j + 1);
+    }
+    //
+    // if (arr.length > 0) {
+    //   console.log(i, arr);
+    // }
+  }
+
+  console.log("SUM", sum);
 }
 
 main();
